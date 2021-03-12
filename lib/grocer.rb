789139a3e78a59786715fb9b6c3e09f20709b5ef
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-03-12 17:37:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,6 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
   item_data = nil 
   collection.each do |data|
     if data[:item] == name
@@ -14,10 +11,6 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   consolidated_cart = []
   cart.each do |item_data|
     item_in_cart = find_item_by_name_in_collection(item_data[:item], consolidated_cart)
@@ -25,23 +18,11 @@ def consolidate_cart(cart)
       new_item = item_data
       new_item[:count]=1
       consolidated_cart << new_item
-      #binding.pry 
     else
       item_in_cart[:count]+=1
-      #binding.pry 
     end 
   end
-  #binding.pry 
   consolidated_cart
 end
-
-grocery_shelf = [
-  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
-  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
-  { :item => "HOT SAUCE", :price => 1.75, :clearance => false },
-  { :item => "TORTILLAS", :price => 2.00, :clearance => false }
-]
-#puts find_item_by_name_in_collection("SALSA", grocery_shelf)
-puts consolidate_cart(grocery_shelf)
 
   
